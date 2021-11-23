a92f63deb344e357be8686f5b9402acbd22f39b1
Jacka Arnold <jackaarnold@outlook.com>
2021-11-23 23:04:44 +1000

    added virus for a threat

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ mutationChance = 100
 lifeTimeMax = 100
 breedablityMax = 1
 
-def cellCombiner(x, y):
-    isMutiant = random.randint(0,mutationChance)
-    if isMutiant = 1:
-        lifeTime = random.randint(0,lifeTimeMax*1.1)
-        if lifeTime > lifeTimeMax:
-            lifeTimeMax = lifeTime
 
 def createCell(parentCell1, parentCell2):
+    isMutiant = random.randint(0,mutationChance)
     location = parentCell1.location
     lifeTime = 100
-    breedablity = cellCombiner(parentCell1,parentCell2,)
+    breedablity = parentCell1,parentCell2
+
+
+def createVirus(parentVirus1, parentVirus2):
+    isMutiant = random.randint(0,mutationChance)
+    location = parentCell1.location
+    lifeTime = 50
+    breedablity = cellCombiner(parentVirus1,parentVirus2)
+
+def createFood():
+    x = random.randint (0,50)
+    y = random.randint (0,50)
 
 
